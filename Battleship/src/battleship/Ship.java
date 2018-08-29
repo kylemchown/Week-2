915af84df5b9d0c630dd4e9dbbb1455c0cc8cffe
kylemchown <kyle.m.chown@gmail.com>
2018-08-29 10:33:24 +0100

    3x3 one ship

---
+++ Battleship/src/battleship/Ship.java
@@ -0,0 +1,19 @@
+package battleship;
+
+public class Ship {
+	public int length;
+	
+	Ship(int length){
+		this.length = length;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+}
